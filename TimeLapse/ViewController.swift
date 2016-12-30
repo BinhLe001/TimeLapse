573fda4baf4498c1777dcd12afb905d4c1de5360
Nam Le <namle91@yahoo.com>
2016-12-30 13:00:37 -0500

    delete nam comment

--- TimeLapse/ViewController.swift
+++ TimeLapse/ViewController.swift
@@ -21,7 +21,6 @@ class ViewController: UIViewController, UIViewControllerTransitioningDelegate {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
-        // Nam Comment
     }
 
     func respondToSwipeGesture(gesture: UIGestureRecognizer) {
